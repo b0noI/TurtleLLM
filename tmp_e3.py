8658ec5c6e7377d6006e1148ce5b51959d2c7b31
Slava Kovalevskyi <vkovalevskyi@google.com>
2024-06-15 14:24:27 -0700

    e3 homework

--- tmp_e3.py
+++ tmp_e3.py
@@ -3,6 +3,7 @@ import jax.numpy as jnp
 import numpy as np
 
 A = jnp.ones((1024, 1024))
+B = jnp.ones((1024, 1024))
 
 jax.debug.visualize_array_sharding(A)
 
@@ -11,7 +12,16 @@ devices2d = np. reshape(jax.devices(), (4, 2))
 mesh = jax.sharding.Mesh(devices2d, ["mainMesh", "mainMesh2"])
 sharding = jax.sharding.NamedSharding(mesh, jax.sharding.PartitionSpec("mainMesh", "mainMesh2"))
 shardedA = jax.device_put(A, sharding)
+shardedB = jax.device_put(B, sharding)
 
-print("======")
+print("===A===")
 
 jax.debug.visualize_array_sharding(shardedA)
+
+print("===B===")
+
+jax.debug.visualize_array_sharding(shardedB)
+
+print("===C===")
+
+jax.debug.visualize_array_sharding(A + B)
